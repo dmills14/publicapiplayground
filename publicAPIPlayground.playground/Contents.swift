08350e44224664d0c50d2cb273fb1d88e5fdc1cb
Drew Miller <drew-miller@lambdastudents.com>
2020-08-03 10:25:30 -0700

    made structs

--- publicAPIPlayground.playground/Contents.swift
+++ publicAPIPlayground.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
-var str = "Hello, playground"
+//Mark: Goals
+//GET all categories - FETCH
+//GET all API's for a specific category - FETCH
+
+let baseURL = URL(string: "https://api.publicapis.org")
+
+struct TopLevelObject {
+    let count: Int
+    let entries: [Entry]
+}
+
+struct Entry {
+    let API: String
+    let Description: String
+    let Auth: String
+    let HTTPS: Bool
+    let Cors: String
+    let Link: String
+    let Category: String
+}
